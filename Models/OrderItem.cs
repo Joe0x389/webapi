d0c7ce8a5aa5f9052e1d24f0d2ba130e024d790e
Youssef <yosefashraf360@gmail.com>
2026-04-25 16:18:54 +0300

    Added user registration and deletion

---
+++ Models/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace webapi.Models;
+
+public class OrderItem
+{
+    public int Id { get; set; }
+    public int DishId { get; set; }
+    public int Quantity { get; set; }
+    public string? Notes { get; set; }
+}
